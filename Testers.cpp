9faec344bfda48f7cec801f81a3754d3aad0c89b
thegoat <ruppatel11599@gmail.com>
2019-12-11 20:29:01 -0500

    SongArrayList testers

--- Testers.cpp
+++ Testers.cpp
@@ -7,19 +7,36 @@
 //
 // Created by Forrest Wargo on 12/11/19.
 //
-void songTesters(){
-    Song* song1 = new Song("eminem", "rap god");
-    Song* song2 = new Song("beatles", "hey jude");
-    Song* song3 = new Song("jay z", "welcome to the jungle");
-    Song* song4 = new Song("beatles", "some song");
 
-    printAssertEquals("rap god",song1->getTitle());
-    //song1->getLength();
-    printAssertEquals("eminem",song1->getArtist());
-    printAssertEquals("beatles",song2->getArtist());
-    printAssertEquals("hey jude",song2->getTitle());
+using namespace std;
+/*
+here comes the sun, beatles, 3:32, 1967
+Billie Jean, Michael Jackson, 5:56, 1980
+rap god, eminem, 5:09, 2013
+lucy in thr sky with diamonds, beatles, 3:45, 1967
+a hard days night, beatles, 3:02, 1964
+loose yourself, eminem, 5:00, 2013
+ */
+void songTesters(){
+    std::cout << "=======SONG TEST=======" <<endl;
+    string song1 = "here comes the sun, beatles, 3:32, 1967";
+    Song* testSong = new Song(song1);
+    printAssertEquals("here comes the sun",testSong->getTitle());
+    printAssertEquals("beatles",testSong->getArtist());
+    printAssertEquals(212,testSong->getLength());
+
+    printAssertEquals(1967,testSong->getYear());
+
+    std::cout << "=======DONE=======" <<endl;
+
+
+
+
+
+
 
 }
+
 
 
 void ArtistMapTesters(){}
@@ -28,6 +45,8 @@ void artistMapNodeTesters(){
 
 }
 void songArrayListTesters(){
+
+    std::cout << "=======SONGARRAYLIST TEST=======" <<endl;
     Song* song1 = new Song("eminem", "rap god");
     Song* song2 = new Song("beatles", "hey jude");
     Song* song3 = new Song("jay z", "welcome to the jungle");
@@ -50,18 +69,14 @@ void songArrayListTesters(){
     songList->clearList();
     printAssertEquals(0, songList->itemCount());
 
-
-
-
-
-
-
-
-
+    std::cout << "=======DONE=======" <<endl;
 }
+
+
 void playlistTesters(){}
 void playListArrayListTesters(){}
 int main(){
+    songTesters();
     songArrayListTesters();
     return 0;
 }
